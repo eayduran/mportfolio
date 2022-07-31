8d967d9c0d19b2f08e5b30f0e9949a168b4e86b8
eayduran <enesayduran@Enes-MacBook-Air.local>
2022-07-31 19:20:20 +0300

    add back to up button

--- components/Main.jsx
+++ components/Main.jsx
@@ -5,7 +5,7 @@ import Navbar from "./Navbar"
 const Main = () => {
 
     return(        
-        <div className="flex flex-col bg-blue-200 h-screen">
+        <div id="main" className="flex flex-col bg-blue-200 h-screen">
             <Navbar />
             <div className="flex justify-center items-center h-screen">
 
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,35 @@
-import Navbar from "../components/Navbar"
 import About from "../components/About"
 import Main from "../components/Main"
 import Contact from "../components/Contact"
 import Projects from "../components/Projects"
 import Skills from "../components/Skills"
+import Upbutton from "../components/Upbutton"
 
+import Link from "next/link"
 import Image from "next/image"
 
+import {useEffect, useState} from 'react';
+
+
 export default function Home() {
+  const [scr, setSrc] = useState(0)
+  useEffect(()=>{
+    const scroll = (event) => {
+      console.log(window.scrollY)
+      setSrc(window.scrollY)
+    }
+    window.addEventListener("scroll", scroll, false);
+    return  () => window.removeEventListener("scroll", scroll, false);
+  },[])
   return (
-    <div className="scroll-smooth scrollbar-thin scrollbar-thumb-blue-700 scrollbar-track-blue-300 h-screen overflow-y-scroll">
+    //  scrollbar-thin scrollbar-thumb-blue-700 scrollbar-track-blue-300 h-screen overflow-y-scroll
+    <div className="">
       <Main />
       <About />
       <Projects />
       <Skills />
       <Contact />
+      <Upbutton scr={scr} />
     </div>
   )
 }
